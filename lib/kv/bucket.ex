de0a7758057b3e1e5c99adec20354ba504e26404
AlexKVal <alexkval@gmail.com>
2016-07-01 22:44:57 +0300

    anonymous fn analogs

--- lib/kv/bucket.ex
+++ lib/kv/bucket.ex
@@ -7,10 +7,12 @@ defmodule KV.Bucket do
   @doc "Get a value from the `bucket` by `key`."
   def get(bucket, key) do
     Agent.get(bucket, &Map.get(&1, key))
+    # Agent.get(bucket, fn x -> Map.get(x, key) end)
   end
 
   @doc "Puts the `value` for the given `key` in the `bucket`."
   def put(bucket, key, value) do
     Agent.update(bucket, &Map.put(&1, key, value))
+    # Agent.update(bucket, fn x -> Map.put(x, key, value) end)
   end
 end
