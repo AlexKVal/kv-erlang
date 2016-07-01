8c16ebb005574a94eae457acbae74eb97fc382a9
AlexKVal <alexkval@gmail.com>
2016-07-01 23:18:07 +0300

    use alias to shorten

--- test/kv/bucket_test.exs
+++ test/kv/bucket_test.exs
@@ -1,25 +1,27 @@
 defmodule KV.BucketTest do
   use ExUnit.Case, async: true
 
+  alias KV.Bucket, as: KVB
+
   setup do
-    {:ok, bucket} = KV.Bucket.start_link
+    {:ok, bucket} = KVB.start_link
     {:ok, bucket: bucket} # put it into the test context
   end
 
   test "stores values by key", %{bucket: bucket} do # match it from the test context
-    assert KV.Bucket.get(bucket, "milk") == nil
+    assert KVB.get(bucket, "milk") == nil
 
-    KV.Bucket.put(bucket, "milk", 3)
-    assert KV.Bucket.get(bucket, "milk") == 3
+    KVB.put(bucket, "milk", 3)
+    assert KVB.get(bucket, "milk") == 3
   end
 
   test "deletes keys and returns their current values", %{bucket: bucket} do
-    assert KV.Bucket.get(bucket, "milk") == nil
-    assert KV.Bucket.delete(bucket, "milk") == nil
+    assert KVB.get(bucket, "milk") == nil
+    assert KVB.delete(bucket, "milk") == nil
 
-    KV.Bucket.put(bucket, "milk", 33)
-    assert KV.Bucket.get(bucket, "milk") == 33
-    assert KV.Bucket.delete(bucket, "milk") == 33
-    assert KV.Bucket.get(bucket, "milk") == nil
+    KVB.put(bucket, "milk", 33)
+    assert KVB.get(bucket, "milk") == 33
+    assert KVB.delete(bucket, "milk") == 33
+    assert KVB.get(bucket, "milk") == nil
   end
 end
