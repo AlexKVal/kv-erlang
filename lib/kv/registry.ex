3500a5ce516cbbae4c45a9da379af488b0ba7fef
AlexKVal <alexkval@gmail.com>
2016-07-02 16:48:42 +0300

    add buckets` monitoring

--- lib/kv/registry.ex
+++ lib/kv/registry.ex
@@ -37,19 +37,37 @@ defmodule KV.Registry do
   ## Server Callbacks
 
   def init(:ok) do
-    {:ok, %{}}
+    names = %{}
+    refs  = %{}
+    {:ok, {names, refs}}
   end
 
-  def handle_call({:lookup, name}, _from, names) do
-    {:reply, Map.fetch(names, name), names}
+  def handle_call({:lookup, name}, _from, {names, _} = state) do
+    {:reply, Map.fetch(names, name), state}
   end
 
-  def handle_cast({:create, name}, names) do
+  def handle_cast({:create, name}, {names, refs} = state) do
     if Map.has_key?(names, name) do
-      {:noreply, names}
+      {:noreply, state}
     else
       {:ok, bucket} = KV.Bucket.start_link
-      {:noreply, Map.put(names, name, bucket)}
+      ref = Process.monitor(bucket)
+      refs = Map.put(refs, ref, name)
+      names = Map.put(names, name, bucket)
+      {:noreply, {names, refs}}
     end
+  end
+
+  def handle_info({:DOWN, ref, :process, _pid, _reason}, {names, refs}) do
+    {name, refs} = Map.pop(refs, ref)
+    names = Map.delete(names, name)
+    {:noreply, {names, refs}}
+  end
+
+  @doc """
+  Discards any unknown message.
+  """
+  def handle_info(_msg, state) do
+    {:noreply, state}
   end
 end
